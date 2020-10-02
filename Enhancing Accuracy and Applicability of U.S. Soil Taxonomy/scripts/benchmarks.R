ea6900301c00a6b3079ea525bae80636364117e4
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2020-10-02 11:22:37 -0700

    template taxonomy

--- Enhancing Accuracy and Applicability of U.S. Soil Taxonomy/scripts/benchmarks.R
+++ Enhancing Accuracy and Applicability of U.S. Soil Taxonomy/scripts/benchmarks.R
@@ -3,7 +3,7 @@ library(soilDB)
 library(tibble)
 
 f <- fetchKSSL(c("davidson","cecil","chewacla"),
-               #c("fayette","tama","downs"), 
+               # c("fayette","tama","downs"),
                  returnMorphologicData = TRUE,
                  returnGeochemicalData = TRUE,
                  simplifyColors = TRUE)
@@ -14,36 +14,38 @@ aqp_df_class(f$SPC) <- "data.table"
 f$SPC <- rebuildSPC(f$SPC)
 
 soildepth <- profileApply(f$SPC, function(p) {
-  
+
   return(tibble(getSoilDepthClass(p)))
-  
+
 }, frameify = TRUE)
 
 head(soildepth)
 
+taxaExtent("", )
+
 argillic <- profileApply(f$SPC, function(p) {
-  
+
   cargi <- t(getArgillicBounds(p))
-  
-  return(tibble(pedon_key = profile_id(p), 
-                featdept = cargi[1], 
+
+  return(tibble(pedon_key = profile_id(p),
+                featdept = cargi[1],
                 featdepb = cargi[2]))
-  
+
 }, frameify = TRUE)
 
 mollic <- profileApply(f$SPC, function(p) {
-  
+
   mss <- getMineralSoilSurfaceDepth(p)
   pld <- getPlowLayerDepth(p)
   if(mss == pld) # no surface horizon present
     mss <- 0
-  cmolli<- t(c(mss, 
+  cmolli<- t(c(mss,
                mss + mollic.thickness.requirement(p, clay.attr = 'clay')))
-  
-  return(tibble(pedon_key = profile_id(p), 
-                featdept = cmolli[1], 
+
+  return(tibble(pedon_key = profile_id(p),
+                featdept = cmolli[1],
                 featdepb = cmolli[2]))
-  
+
 }, frameify = TRUE)
 
 mollic$featkind <- "mollic calculated"
